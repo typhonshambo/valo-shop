da45f5c8b78fc89664b29994e4e84f077dd7dae8
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2022-08-13 11:02:18 +0530

    UI changes

--- commands/commands/login.py
+++ commands/commands/login.py
@@ -6,6 +6,12 @@ from .utils.shopData import *
 import json
 import nest_asyncio
 
+with open ('././config/emoji.json', 'r') as f:
+	emojidata = json.load(f)
+
+with open ('././config/config.json', 'r') as f:
+	data = json.load(f)
+	guilds = data['guilds']
 
 
 class LOGIN(commands.Cog):
@@ -34,7 +40,10 @@ class LOGIN(commands.Cog):
 					color = discord.Color.random(),
 					description="> Successfully linked"
 				)
-				await ctx.respond(embed=embed)
+				view = discord.ui.View()
+				view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
+				view.add_item(discord.ui.Button(label='Github', url='https://github.com/typhonshambo/valo-shop', style=discord.ButtonStyle.url, emoji=emojidata["github"]))
+				await ctx.respond(embed=embed, view=view)
 			
 			if user:
 				await self.bot.pg_con.execute("UPDATE shopDB SET username = $1, password = $2, region=$3, access_token=$4, entitlements_token=$5, ingameUserID=$6 WHERE user_id = $7",username, password, userData[3], userData[0], userData[1], userData[2],author_id)
@@ -42,13 +51,19 @@ class LOGIN(commands.Cog):
 					color = discord.Color.random(),
 					description="> Successfully linked"
 				)
-				await ctx.respond(embed=embed)
+				view = discord.ui.View()
+				view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
+				view.add_item(discord.ui.Button(label='Github', url='https://github.com/typhonshambo/valo-shop', style=discord.ButtonStyle.url, emoji=emojidata["github"]))
+				await ctx.respond(embed=embed, view=view)
 		except:
 			embed= discord.Embed(
 				color=discord.Color.red(),
 				description="> Wrong Username or Password provided!"
 			)
-			await ctx.respond(embed=embed)
+			view = discord.ui.View()
+			view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
+			view.add_item(discord.ui.Button(label='Github', url='https://github.com/typhonshambo/valo-shop', style=discord.ButtonStyle.url, emoji=emojidata["github"]))
+			await ctx.respond(embed=embed, view=view)
 
 
 def setup(client):
