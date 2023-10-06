d9faa450e4673ae366e42ef3e824f740f7498188
sammyBrokeTheWeb <114849696+sammyBrokeTheWeb@users.noreply.github.com>
2023-10-06 21:25:05 +0000

    Update invite.py
    - added UPI link

--- commands/commands/invite.py
+++ commands/commands/invite.py
@@ -7,17 +7,13 @@ import json
 with open ('././config/emoji.json', 'r') as f:
 	emojidata = json.load(f)
 
-with open ('././config/config.json', 'r') as f:
-	data = json.load(f)
-	guilds = data['guilds']
-
 
 class INVITE(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
 
-	@commands.slash_command(description="Invite the BOT in your server", guild_ids=guilds)
+	@commands.slash_command(description="Invite the BOT in your server")
 	async def invite(
 		self,
 		ctx
@@ -29,6 +25,7 @@ class INVITE(commands.Cog):
 		)
 		view = discord.ui.View()
 		view.add_item(discord.ui.Button(label='Invite', url='https://discord.com/api/oauth2/authorize?client_id=980918916211695717&permissions=139586816064&scope=bot%20applications.commands', style=discord.ButtonStyle.url, emoji=emojidata["invite"]))
+		view.add_item(discord.ui.Button(label='Donation', url='https://upilinks.in/payment-link/upi1049540315', style=discord.ButtonStyle.url, emoji='💰'))
 		await ctx.respond(embed=embed, view=view)
 		
 	
