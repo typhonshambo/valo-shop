0c1b9e08603adfb9cf2413766904e554d9103077
sammyBrokeTheWeb <114849696+sammyBrokeTheWeb@users.noreply.github.com>
2023-10-06 21:27:43 +0000

    Update login.py
    - added command for logout
    - msg given that after 1 hr player need to login again

--- commands/commands/login.py
+++ commands/commands/login.py
@@ -9,15 +9,13 @@ import json
 with open ('././config/emoji.json', 'r') as f:
 	emojidata = json.load(f)
 
-with open ('././config/config.json', 'r') as f:
-	data = json.load(f)
-	guilds = data['guilds']
+
 
 class LOGIN(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.slash_command(description="Link to your Valorant account", guild_ids=guilds)
+	@commands.slash_command(description="Link to your Valorant account")
 	async def login(
 		self,
 		ctx,
@@ -39,20 +37,24 @@ class LOGIN(commands.Cog):
 					color = discord.Color.random(),
 					description="> Successfully linked"
 				)
+				embed.set_footer(text=("After 1 hr you need to login again!"))
 				view = discord.ui.View()
 				view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
-				view.add_item(discord.ui.Button(label='Github', url='https://github.com/typhonshambo/valo-shop', style=discord.ButtonStyle.url, emoji=emojidata["github"]))
+				view.add_item(discord.ui.Button(label='Donation', url='https://upilinks.in/payment-link/upi1049540315', style=discord.ButtonStyle.url, emoji='💰'))
 				await ctx.respond(embed=embed, view=view, ephemeral=True)
 			
 			if user:
 				await self.bot.pg_con.execute("UPDATE shopDB SET username = $1, password = $2, region=$3, access_token=$4, entitlements_token=$5, ingameUserID=$6 WHERE user_id = $7",username, password, userData[3], userData[0], userData[1], userData[2],author_id)
 				embed = discord.Embed(
 					color = discord.Color.random(),
-					description="> Successfully linked"
+					description="> Successfully linked!"
+					
 				)
+				embed.set_footer(text=("After 1 hr you need to login again!"))
+
 				view = discord.ui.View()
 				view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
-				view.add_item(discord.ui.Button(label='Github', url='https://github.com/typhonshambo/valo-shop', style=discord.ButtonStyle.url, emoji=emojidata["github"]))
+				view.add_item(discord.ui.Button(label='Donation', url='https://upilinks.in/payment-link/upi1049540315', style=discord.ButtonStyle.url, emoji='💰'))
 				await ctx.respond(embed=embed, view=view)
 		except:
 			embed= discord.Embed(
@@ -61,8 +63,48 @@ class LOGIN(commands.Cog):
 			)
 			view = discord.ui.View()
 			view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
-			view.add_item(discord.ui.Button(label='Github', url='https://github.com/typhonshambo/valo-shop', style=discord.ButtonStyle.url, emoji=emojidata["github"]))
+			view.add_item(discord.ui.Button(label='Donation', url='https://upilinks.in/payment-link/upi1049540315', style=discord.ButtonStyle.url, emoji='💰'))
 			await ctx.respond(embed=embed, view=view)
+
+	@commands.slash_command(description="Unlink to your Valorant account")
+	async def logout(
+		self,
+		ctx
+	):
+		await ctx.response.defer(ephemeral=True)
+
+		try:
+			author_id = str(ctx.author.id)
+			user = await self.bot.pg_con.fetchrow("SELECT * FROM shopDB WHERE user_id = $1", author_id)
+			if not user:
+				embed= discord.Embed(
+					color=discord.Color.red(),
+					description="> Login to continue, use `/login`"
+				)
+				view = discord.ui.View()
+				view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
+				view.add_item(discord.ui.Button(label='Donation', url='https://upilinks.in/payment-link/upi1049540315', style=discord.ButtonStyle.url, emoji='💰'))
+				await ctx.respond(embed=embed, view=view)
+			if user:
+				await self.bot.pg_con.fetchval(
+					"DELETE FROM shopDB WHERE user_id = $1", 
+					author_id
+				)
+				embed = discord.Embed(
+						color = discord.Color.green(),
+						description="> Successfully unlinked"
+				)
+				await ctx.respond(embed=embed)
+		except:
+			embed= discord.Embed(
+				color=discord.Color.red(),
+				description="> Login to continue, use `/login`"
+			)
+			view = discord.ui.View()
+			view.add_item(discord.ui.Button(label='Support Server', url='https://discord.gg/m5mSyTV7RR', style=discord.ButtonStyle.url, emoji=emojidata["support"]))
+			view.add_item(discord.ui.Button(label='Donation', url='https://upilinks.in/payment-link/upi1049540315', style=discord.ButtonStyle.url, emoji='💰'))
+			await ctx.respond(embed=embed, view=view)
+
 
 
 def setup(client):
